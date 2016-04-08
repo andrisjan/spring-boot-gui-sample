ef0e8128637a11546094a2945cce9761a34d98d7
Andris <andris.jansons@gmail.com>
2016-04-08 22:50:10 +0300

    Maven compiler plugin added

--- src/main/java/lv/home/test/Runner.java
+++ src/main/java/lv/home/test/Runner.java
@@ -2,15 +2,19 @@ package lv.home.test;
 
 import lv.home.test.gui.MainForm;
 import lv.home.test.service.AppInfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 @Component
 public class Runner implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(Runner.class);
 
     @Autowired
     private MainForm mainForm;
@@ -20,6 +24,17 @@ public class Runner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        }
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
         /* display the form using the AWT EventQueue */
         java.awt.EventQueue.invokeLater(new Runnable() {
 
@@ -45,24 +60,5 @@ public class Runner implements CommandLineRunner {
         }
     }
 
-    //    public static void main(String args[]) {
-//        try {
-//            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-//            LOG.error(ex.getMessage(), ex);
-//        }
-//
-//        /* Create and display the form */
-//        EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                new MainForm().setVisible(true);
-//            }
-//        });
-//    }
 
 }
